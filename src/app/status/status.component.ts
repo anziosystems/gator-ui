a01c0f7025f0c645b79239066ef988adc93bd2c8
Rafat Sarosh <rsarosh@hotmail.com>
2020-02-07 13:06:50 -0500

    CSS and remove etc call of GetOrg

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -32,7 +32,7 @@ export class StatusComponent implements OnInit {
     @Inject(SESSION_STORAGE) private sessionStorage: WebStorageService,
   ) {
     this.bGetFromGit = sessionStorage.get('LBC');
-    
+
     this.loginAndSetup();
   }
 
@@ -64,151 +64,141 @@ export class StatusComponent implements OnInit {
       }
     }, 200); //every 200 ms
 
-    this.gitService
-      .getOrgList(
-        this.bGetFromGit, //Get from Git
-        this.bGetFromGit,
-      )
-      .subscribe(
-        //superflaus call, just waking up the sleeping API
-        res => {
-          //Get the Tenant details - This will be logged in User
-          this.gitService.getGitLoggedInUSerDetails(this.bGetFromGit).subscribe(result => {
-            let dd = new DevDetails();
-            dd.name = result.DisplayName;
-            dd.login = result.UserName;
-            dd.image = result.Photo;
-            dd.id = result.Id;
-            dd.profileUrl = result.profileUrl;
-            let buff = btoa(JSON.stringify(dd));
-            this.gitService.setLoggedInGitDev(dd);
-          });
+    //Get the Tenant details - This will be logged in User
+    this.gitService.getGitLoggedInUSerDetails(this.bGetFromGit).subscribe(result => {
+      let dd = new DevDetails();
+      dd.name = result.DisplayName;
+      dd.login = result.UserName;
+      dd.image = result.Photo;
+      dd.id = result.Id;
+      dd.profileUrl = result.profileUrl;
+      let buff = btoa(JSON.stringify(dd));
+      this.gitService.setLoggedInGitDev(dd);
+    });
 
-          this.hookFail = false;
+    this.hookFail = false;
+    if (this.bGetFromGit) {
+      this.messages.push('Please wait, getting Org List ...');
+    }
+
+    this.gitService.getOrgList(this.bGetFromGit, this.bGetFromGit).subscribe(
+      result => {
+        if (result.length > 0) {
+          this.orgStatus = true;
+          this.orgList = result;
+          this.gitService.setCurrentOrg(this.orgList[0].Org); //setting the default
+
           if (this.bGetFromGit) {
-            this.messages.push('Please wait, getting Org List ...');
+            this.successMessages.push(`Yes! Found ${result.length} orgnization for this login`);
           }
-
-          this.gitService.getOrgList(this.bGetFromGit, this.bGetFromGit).subscribe(
-            result => {
-              if (result.length > 0) {
-                this.orgStatus = true;
-                this.orgList = result;
-                this.gitService.setCurrentOrg(this.orgList[0].Org); //setting the default
-               
+          //for every org check the hook
+          this.orgList.forEach(element => {
+            if (this.bGetFromGit) {
+              this.messages.push('Checking CTODash hook in ' + element.Org);
+            }
+            this.gitService.getHookStatus(element.Org).subscribe(result => {
+              let hookStatus = result.val;
+              if (!hookStatus) {
+                //lets install the hook
                 if (this.bGetFromGit) {
-                  this.successMessages.push(`Yes! Found ${result.length} orgnization for this login`);
+                  this.messages.push('Installing web hook in ' + element.Org);
                 }
-                //for every org check the hook
-                this.orgList.forEach(element => {
-                  if (this.bGetFromGit) {
-                    this.messages.push('Checking CTODash hook in ' + element.Org);
-                  }
-                  this.gitService.getHookStatus(element.Org).subscribe(result => {
-                    let hookStatus = result.val;
-                    if (!hookStatus) {
-                      //lets install the hook
+                this.gitService.setupWebHook(element.Org).subscribe(result => {
+                  let hookReturn = result.val;
+                  if (hookReturn === 201) {
+                    if (this.bGetFromGit) {
+                      this.successMessages.push('CTODash hook is installed for org ' + element.Org);
+                    }
+                  } else {
+                    if (hookReturn === 422) {
                       if (this.bGetFromGit) {
-                        this.messages.push('Installing web hook in ' + element.Org);
+                        this.messages.push('CTODash hook is already installed for org ' + element.Org);
                       }
-                      this.gitService.setupWebHook(element.Org).subscribe(result => {
-                        let hookReturn = result.val;
-                        if (hookReturn === 201) {
-                          if (this.bGetFromGit) {
-                            this.successMessages.push('CTODash hook is installed for org ' + element.Org);
-                          }
-                        } else {
-                          if (hookReturn === 422) {
-                            if (this.bGetFromGit) {
-                              this.messages.push('CTODash hook is already installed for org ' + element.Org);
-                            }
-                          } else {
-                            if (hookReturn === 404) {
-                              if (this.bGetFromGit) {
-                                this.errMessages.push("Couldn't install CTODash hook. Please install manually for org: " + element.Org);
-                                this.hookFail = true;
-                              }
-                            }
-                          }
-                        }
-                      });
                     } else {
-                      if (this.bGetFromGit) {
-                        this.messages.push('CTODash hook is already installed in ' + element.Org);
+                      if (hookReturn === 404) {
+                        if (this.bGetFromGit) {
+                          this.errMessages.push("Couldn't install CTODash hook. Please install manually for org: " + element.Org);
+                          this.hookFail = true;
+                        }
                       }
                     }
-                  });
-                  //Get Repos
-                  if (this.bGetFromGit) {
-                    this.messages.push('Please Wait! Getting Repositories for ' + element.Org);
                   }
-                  this.gitService.getRepoList(element.Org, this.bGetFromGit, this.bGetFromGit).subscribe(
-                    result => {
-                      //TODO: Turn the result into true and false
-                      if (result.length > 0) {
-                        this.repoStatus = true;
-                        this.repoCount = result.length;
-                        if (this.bGetFromGit) {
-                          this.successMessages.push('Found Repositories: ' + result.length + ' for ' + element.Org);
-                        }
-                      } else {
-                        if (this.bGetFromGit) {
-                          this.warningMessages.push('No Repositories found for organization: ' + element.Org);
-                        }
-                      }
-                    },
-                    error => {
-                      this.errMessages.push('Sorry, seems like something is wrong getting repository list. Please refresh the page. ');
-                      this.errMessages.push(error.statusText);
-                      clearTimeout(t);
-                    },
-                  );
-                  if (this.bGetFromGit) {
-                    this.messages.push('Getting last 10 pull request from all repositories for ' + element.Org + ' Please wait ..');
-                  }
-
-                  //Get Pull Request
-                  this.gitService.getPullRequest(element.Org, this.bGetFromGit, this.bGetFromGit).subscribe(
-                    result => {
-                      //TODO: Turn the result into true and false
-                      if (this.bGetFromGit) {
-                        this.successMessages.push('Done! Getting pull request for ' + element.Org + ' from ' + result + ' repositories');
-                      }
-                      this.buttonDisabled = false;
-                      let elem = document.getElementById('myBar');
-                      elem.style.width = '100%';
-                      clearTimeout(t);
-                    //  this.router.navigate(['/dashboard']);  //No Need for user to click
-                    },
-                    error => {
-                      if (this.bGetFromGit) {
-                        this.errMessages.push('Sorry, seems like something is wrong getting PR. Please refresh the page. ');
-                        this.errMessages.push(error.statusText);
-                      }
-                      clearTimeout(t);
-                    },
-                  );
-                }); //org list loop
+                });
               } else {
                 if (this.bGetFromGit) {
-                  this.warningMessages.push('Did not get any orgnazation for this login. Please check in Git and make sure you belong to an organization.');
-                  this.warningMessages.push('Exiting!!!');
+                  this.messages.push('CTODash hook is already installed in ' + element.Org);
                 }
+              }
+            });
+            //Get Repos
+            if (this.bGetFromGit) {
+              this.messages.push('Please Wait! Getting Repositories for ' + element.Org);
+            }
+            this.gitService.getRepoList(element.Org, this.bGetFromGit, this.bGetFromGit).subscribe(
+              result => {
+                //TODO: Turn the result into true and false
+                if (result.length > 0) {
+                  this.repoStatus = true;
+                  this.repoCount = result.length;
+                  if (this.bGetFromGit) {
+                    this.successMessages.push('Found Repositories: ' + result.length + ' for ' + element.Org);
+                  }
+                } else {
+                  if (this.bGetFromGit) {
+                    this.warningMessages.push('No Repositories found for organization: ' + element.Org);
+                  }
+                }
+              },
+              error => {
+                this.errMessages.push('Sorry, seems like something is wrong getting repository list. Please refresh the page. ');
+                this.errMessages.push(error.statusText);
+                clearTimeout(t);
+              },
+            );
+            if (this.bGetFromGit) {
+              this.messages.push('Getting last 10 pull request from all repositories for ' + element.Org + ' Please wait ..');
+            }
+
+            //Get Pull Request
+            this.gitService.getPullRequest(element.Org, this.bGetFromGit, this.bGetFromGit).subscribe(
+              result => {
+                //TODO: Turn the result into true and false
+                if (this.bGetFromGit) {
+                  this.successMessages.push('Done! Getting pull request for ' + element.Org + ' from ' + result + ' repositories');
+                }
+                this.buttonDisabled = false;
                 let elem = document.getElementById('myBar');
                 elem.style.width = '100%';
                 clearTimeout(t);
-              }
-            },
-            error => {
-              if (this.bGetFromGit) {
-                this.errMessages.push('Sorry, seems like something is wrong. Please refresh the page. Please feel free to send us message at support@anziosystems.com ');
-                this.errMessages.push(error.message);
-              }
-              clearTimeout(t);
-            },
-          );
-        },
-      );
+                //  this.router.navigate(['/dashboard']);  //No Need for user to click
+              },
+              error => {
+                if (this.bGetFromGit) {
+                  this.errMessages.push('Sorry, seems like something is wrong getting PR. Please refresh the page. ');
+                  this.errMessages.push(error.statusText);
+                }
+                clearTimeout(t);
+              },
+            );
+          }); //org list loop
+        } else {
+          if (this.bGetFromGit) {
+            this.warningMessages.push('Did not get any orgnazation for this login. Please check in Git and make sure you belong to an organization.');
+            this.warningMessages.push('Exiting!!!');
+          }
+          let elem = document.getElementById('myBar');
+          elem.style.width = '100%';
+          clearTimeout(t);
+        }
+      },
+      error => {
+        if (this.bGetFromGit) {
+          this.errMessages.push('Sorry, seems like something is wrong. Please refresh the page. Please feel free to send us message at support@anziosystems.com ');
+          this.errMessages.push(error.message);
+        }
+        clearTimeout(t);
+      },
+    );
   }
   ngOnInit() {}
 
