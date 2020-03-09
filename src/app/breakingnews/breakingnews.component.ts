0b460515ee666c8090a7cfa736185ba8b01811eb
Rafat Sarosh <rsarosh@hotmail.com>
2020-03-09 07:19:27 -0400

    Fixing Breaking News for Full Name

--- src/app/breakingnews/breakingnews.component.ts
+++ src/app/breakingnews/breakingnews.component.ts
@@ -23,18 +23,21 @@ export class BreakingnewsComponent implements OnInit {
     this.updateBreakingNews();
   }
 
-  getData() {
-    this.gitService.ready().then(result => {
-      this.gitService.getDeveloperDetail(this.gitService.getCurrentOrg(), 7, null, null, 50).subscribe(val => {
-        this.devDetails = val;
-        this.devDetails.map(v => {
-          let s = v.pullrequesturl;
-          s = s.replace('https://api.github.com/repos', 'https://github.com');
-          s = s.replace('pulls', 'pull');
-          s = s.replace('comments', ' ');
-          v.pullrequesturl = s;
-          v.body = v.body.replace(/\+/g, ' ');
-          v.title = v.title.replace(/\+/g, ' ');
+  getData(): Promise<boolean> {
+    return new Promise((done, fail) => {
+      this.gitService.ready().then(result => {
+        this.gitService.getDeveloperDetail(this.gitService.getCurrentOrg(), 7, null, null, 50).subscribe(val => {
+          this.devDetails = val;
+          this.devDetails.map(v => {
+            let s = v.pullrequesturl;
+            s = s.replace('https://api.github.com/repos', 'https://github.com');
+            s = s.replace('pulls', 'pull');
+            s = s.replace('comments', ' ');
+            v.pullrequesturl = s;
+            v.body = v.body.replace(/\+/g, ' ');
+            v.title = v.title.replace(/\+/g, ' ');
+            done(true);
+          });
         });
       });
     });
@@ -42,19 +45,24 @@ export class BreakingnewsComponent implements OnInit {
 
   updateBreakingNews() {
     this.breakingNews = 'Breaking news will come here..';
-    this.getData();
-    this.timer = setInterval(() => {
-      this.ctr = this.ctr + 1;
-      if (this.ctr > this.devDetails.length - 1) {
-        this.ctr = 0;
-        this.getData();
-      }
-      this.login = this.devDetails[this.ctr].Login;
-      this.repo = this.devDetails[this.ctr].Repo;
-      this.desc = this.devDetails[this.ctr].title;
-      this.pullReqUrl = this.devDetails[this.ctr].pullrequesturl;
-      this.breakingNews = `${this.desc} - ${this.login} - ${this.repo}`;
-      this.cd.detectChanges();
-    }, 8000);
+    this.getData().then(() => {
+      this.timer = setInterval(() => {
+        this.ctr = this.ctr + 1;
+        if (!this.devDetails) return;
+        if (this.ctr > this.devDetails.length - 1) {
+          this.ctr = 0;
+          this.getData();
+        }
+        this.login = this.devDetails[this.ctr].Login;
+        this.gitService.getGitDisplayName4Login(this.login).then(r => {
+          this.login = r;
+          this.repo = this.devDetails[this.ctr].Repo;
+          this.desc = this.devDetails[this.ctr].title;
+          this.pullReqUrl = this.devDetails[this.ctr].pullrequesturl;
+          this.breakingNews = `${this.desc} - ${this.login} - ${this.repo}`;
+          this.cd.detectChanges();
+        });
+      }, 8000);
+    });
   }
 }
