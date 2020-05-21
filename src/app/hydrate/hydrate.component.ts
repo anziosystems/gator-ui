04357c3a04617af4a86a81aaa488eb2eb89fea9d
Rafat Sarosh <rsarosh@hotmail.com>
2020-05-20 23:02:15 -0700

    Added Admin check on Hydration

--- src/app/hydrate/hydrate.component.ts
+++ src/app/hydrate/hydrate.component.ts
@@ -7,19 +7,47 @@ import {SESSION_STORAGE, WebStorageService} from 'ngx-webstorage-service';
 @Component({
   selector: 'app-hydrate',
   templateUrl: './hydrate.component.html',
-  styleUrls: ['./hydrate.component.less']
+  styleUrls: ['./hydrate.component.less'],
 })
 export class HydrateComponent implements OnInit {
+  constructor(private router: Router, location: Location, private gitService: GitService, @Inject(SESSION_STORAGE) private sessionStorage: WebStorageService) {}
 
-  constructor(private router: Router, location: Location, private gitService: GitService, @Inject(SESSION_STORAGE) private sessionStorage: WebStorageService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async gitLogin() {
+    let loggedUser = this.gitService.getLoggedInDev().Login;
+    let currentOrg = await this.gitService.getCurrentOrg();
+    this.gitService.isUserAdmin(currentOrg, loggedUser).subscribe(x => {
+      if (x === 0) {
+        alert('Sorry, only admin can see this');
+      } else {
+        const authURL = this.gitService.gatorApiUrl + '/auth/github?callbackUrl=' + location.origin + '/callback';
+        window.location.href = authURL;
+      }
+    });
   }
 
-  gitLogin() {
-    const authURL = this.gitService.gatorApiUrl + '/auth/github?callbackUrl=' + location.origin + '/callback';
+  async jiraLogin() {
+    let loggedUser = this.gitService.getLoggedInDev().Login;
+    let currentOrg = await this.gitService.getCurrentOrg();
+    this.gitService.isUserAdmin(currentOrg, loggedUser).subscribe(x => {
+      if (x === 0) {
+        alert('Sorry, only admin can see this');
+      } else {
+        this.router.navigate(['/jira-login']);
+      }
+    });
+  }
 
-    //this.router.navigateByUrl(authURL);
-    window.location.href = authURL;
+  async tfsLogin() {
+    let loggedUser = this.gitService.getLoggedInDev().Login;
+    let currentOrg = await this.gitService.getCurrentOrg();
+    this.gitService.isUserAdmin(currentOrg, loggedUser).subscribe(x => {
+      if (x === 0) {
+        alert('Sorry, only admin can see this');
+      } else {
+        this.router.navigate(['/tfs-login']);
+      }
+    });
   }
 }
